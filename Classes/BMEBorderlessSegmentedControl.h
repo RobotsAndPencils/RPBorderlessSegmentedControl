91df8c228baf9b513abe9b651979b2902d0e01d3
Brandon Evans <brandon@brandonevans.ca>
2014-02-10 23:41:56 -0700

    Clean up the Xcode project

--- Classes/BMEBorderlessSegmentedControl.h
+++ Classes/BMEBorderlessSegmentedControl.h
@@ -1,12 +1,10 @@
 //
 //  BMEBorderlessSegmentedControl.h
-//  
-//
-//  Created by Brandon on 2/10/2014.
 //
 //
-
-#import <Cocoa/Cocoa.h>
+//  Created by Brandon Evans on 2/10/2014.
+//  Copyright (c) 2014 Brandon Evans. All rights reserved.
+//
 
 @interface BMEBorderlessSegmentedControl : NSSegmentedControl
 
